6b4577aeaf94e2f6f39ac36d47d85c568fd16678
iris.dumeur6@etu.univ-lorraine.fr <iris.dumeur6@etu.univ-lorraine.fr>
2024-12-12 16:11:39 +0100

    TEST: temporal PE

---
+++ tests/test_temporal_positional_encoder.py
@@ -0,0 +1,10 @@
+from alise_minimal.torch_model.temporal_positional_encoder import PositionalEncoder
+import torch
+
+
+def test_forward():
+    B, T, d_model = 2, 30, 32
+    input = torch.rand(B, T)
+    pe = PositionalEncoder(d=d_model, T=3000)
+    output = pe(input)
+    assert output.shape == (B, T, d_model)
