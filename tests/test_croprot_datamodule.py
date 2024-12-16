21ab2cfd4ea792aa91c975d9e7c0bdc6c722287a
Iris DUMEUR <dumeuri@trexvisu04.sis.cnes.fr>
2024-12-16 15:56:30 +0000

    FIX MERGE

--- tests/test_croprot_datamodule.py
+++ tests/test_croprot_datamodule.py
@@ -1,6 +1,5 @@
 import pytest
 import torch
-
 from alise_minimal.data.batch_class import CDBInput
 from alise_minimal.data.datamodule.croprot_datamodule import CropRotDataModule
 
@@ -9,10 +8,12 @@ from alise_minimal.data.datamodule.croprot_datamodule import CropRotDataModule
 def test_croprot_datamodule():
     PATH = "/home/ad/dumeuri/DeepChange/PASTIS_CD/PASTIS_CD/PASTIS_PT_MASK_SHARE2"
     path_dir_csv = "/home/ad/dumeuri/DeepChange/MMDC_OE"
-    datamodule = CropRotDataModule(
-        dataset_path=PATH, path_dir_csv=path_dir_csv, batch_size=2
-    )
+
+    datamodule = CropRotDataModule(dataset_path=PATH,
+                                   path_dir_csv=path_dir_csv,
+                                   batch_size=2)
     batch: CDBInput = next(iter(datamodule.train_dataloader()))
     assert batch.year1.sits.shape[0] == 2
     assert batch.year1.sits.shape[1] == batch.year1.positions.shape[1]
-    assert torch.min(batch.year1.positions) - torch.max(batch.year2.positions) < 2 * 365
+    assert torch.min(batch.year1.positions) - torch.max(
+        batch.year2.positions) < 2 * 365
