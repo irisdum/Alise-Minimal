91c4d67b7011f76c514006ec3f2bafb3d5bf0c7a
iris.dumeur6@etu.univ-lorraine.fr <iris.dumeur6@etu.univ-lorraine.fr>
2024-12-16 16:56:04 +0100

    FIX:  croprot datamodule test

--- tests/test_croprot_datamodule.py
+++ tests/test_croprot_datamodule.py
@@ -1,8 +1,8 @@
 import pytest
 import torch
 
+from alise_minimal.data.batch_class import CDBInput
 from alise_minimal.data.datamodule.croprot_datamodule import CropRotDataModule
-from alise_minimal.data.dataset.sample_class import CDInput
 
 
 @pytest.mark.trex
@@ -12,10 +12,7 @@ def test_croprot_datamodule():
     datamodule = CropRotDataModule(
         dataset_path=PATH, path_dir_csv=path_dir_csv, batch_size=2
     )
-    batch: CDInput = next(iter(datamodule.train_dataloader()))
-    assert batch.year1.s2.sits.shape[0] == 2
-    assert batch.year1.s2.sits.shape[1] == batch.year1.s2.positions[1]
-    assert (
-        torch.min(batch.year1.s2.positions) - torch.max(batch.year2.s2.positions)
-        < 2 * 365
-    )
+    batch: CDBInput = next(iter(datamodule.train_dataloader()))
+    assert batch.year1.sits.shape[0] == 2
+    assert batch.year1.sits.shape[1] == batch.year1.positions.shape[1]
+    assert torch.min(batch.year1.positions) - torch.max(batch.year2.positions) < 2 * 365
